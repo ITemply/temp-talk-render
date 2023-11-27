95bee62887966518ed7b6c7a5f9c9740f23c8ca0
ITemply <120138779+ITemply@users.noreply.github.com>
2023-11-27 08:19:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
 
-// Define the target URL you want to proxy
-const targetUrl = 'https://atlantic.onrender.com'; // Replace with the target URL you want to proxy
+const targetUrl = 'https://temptalk.onrender.com';
 
-// Create a proxy middleware
 const proxy = createProxyMiddleware({
   target: targetUrl,
-  changeOrigin: true, // Necessary for host header and other proxy headers to be set correctly
-  // Add any other options you need, like pathRewrite, etc.
+  changeOrigin: true,
 });
 
-// Use the proxy middleware for all incoming requests
 app.use('/', proxy);
 
 app.listen(port, () => {
